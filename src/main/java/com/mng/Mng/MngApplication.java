4665ed4849a4a95d38665b53eef156559374a8dc
Vadim Kiniabaev <thekinv21@gmail.com>
2024-03-15 23:21:40 +0300

    FEAT: project created

---
+++ src/main/java/com/mng/Mng/MngApplication.java
@@ -0,0 +1,13 @@
+package com.mng.Mng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MngApplication.class, args);
+	}
+
+}
